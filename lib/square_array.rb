9f950bef6a08677bd7ce43d23adb1bec4b5181b3
Sarah Rose <sarahrose63089@gmail.com>
2020-06-04 16:34:13 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  count = 0
+  new_array = []
+  while array[count] do
+    new_item = array[count]**2
+    new_array << new_item
+  end
+  new_array
 end
